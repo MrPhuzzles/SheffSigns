161b696d6b18fe21ebeba1af88bd0cbffc3ea5c0
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-11-02 22:51:10 -0400

    Added Navbar container.
    Added the basic navbar ready to set up 3 routes to start.

--- sheff-client/src/containers/App.js
+++ sheff-client/src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {Provider} from "react-redux";
 import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./Navbar";
 
 const App = () => (
   <Router>
   <div>
+    <Navbar />
   </div>
   </Router>
 );
