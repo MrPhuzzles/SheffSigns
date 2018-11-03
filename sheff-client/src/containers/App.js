cdb95e9bb2d39988ee66868312b148e8e5a5bc5d
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-11-02 22:21:56 -0400

    Sorted folders to start adding components
    Sorted my folders in sheff-client for easier addition of components and routes.

---
+++ sheff-client/src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
+
+const App = () => (
+  <Router>
+  <div>
+  </div>
+  </Router>
+);
+
+export default App;
