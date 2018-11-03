a92204ffa1a8c416aeb43ddb10f16276ec76bb0b
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-11-03 15:18:56 -0400

    Added minor homepage text
    Added company name and possible slogan to top of homepage for positioning.

--- sheff-client/src/components/Homepage.js
+++ sheff-client/src/components/Homepage.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+
 const Homepage = () => {
   return (
     <div>
-      <h1>Homepage goes here!</h1>
+      <div id="homeTop">
+        <h1>SHEFFIELD SIGNS</h1>   {/* to be logo! */}
+        <h2>A professional and innovative solution to your sign needs!</h2>
+      </div>
     </div>
   );
 };
