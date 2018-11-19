d7d3494618f1540acc355fdb0170ea42b48f171e
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-11-18 19:26:16 -0500

    Updated Contact
    Added bottom feedback piece.

--- sheff-client/src/components/Contact.js
+++ sheff-client/src/components/Contact.js
@@ -3,17 +3,22 @@ import React from "react";
 const Contact = () => {
   return (
     <div class="container">
-    <div class="row">
-      <div class="col-sm">
-        Phone Inquiries
+      <div class="row">
+        <div class="col-sm">
+          Phone Inquiries
+        </div>
+        <div class="col-sm">
+          Email Inquiries
+        </div>
+        <div class="col-sm">
+          Link to Facebook Page
+        </div>
       </div>
-      <div class="col-sm">
-        Email Inquiries
+      <div class="row justify-content-center">
+        <div class="col">
+          Feedback Info
+        </div>
       </div>
-      <div class="col-sm">
-        Link to Facebook Page
-      </div>
-    </div>
   </div>
   );
 };
