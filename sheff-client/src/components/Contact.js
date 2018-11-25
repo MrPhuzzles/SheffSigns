df69c67ffed95b5367dda649f370b89588c8a7a0
MrPhuzzles <tietjen.james92@gmail.com>
2018-11-25 16:24:01 -0500

    Updated contact page
    Converted contact.js to use just flex instead of grid layout. More reliable for all browsers. Also styled the text sizing and added in functional link tags for Facebook and email.

--- sheff-client/src/components/Contact.js
+++ sheff-client/src/components/Contact.js
@@ -3,25 +3,29 @@ import React from "react";
 const Contact = () => {
   return (
   <div class="container">
-    <div id="ContactHead">
-    <h1>Here's how you can get in touch with us...</h1>
-    </div>
-      <div class="row">
-        <div class="col-4">
+      <div id="ContactHead" class="d-flex p-2 bd-highlight justify-content-center">
+        <h1>Here's how you can get in touch with us:</h1>
+      </div>
+      <div id="InquireMethods" class="d-flex bd-highlight">
+        <div class="p-2 flex-even bd-highlight">
           <h2>Give us a call!</h2>
+          <h5>You can call us at 289-716-4208</h5>
+          <h5>7 days a week before 6pm</h5>
         </div>
-        <div class="col-4">
+        <div class="p-2 flex-even bd-highlight">
           <h2>Send us an email!</h2>
+          <h5>General inquiries can also be made via the email below</h5>
+          <h5><a href="mailto:sheffieldsigns@yahoo.ca">sheffieldsigns@yahoo.ca</a></h5>
         </div>
-        <div class="col-4">
-          <h2>Find us on Facebook!</h2>
+        <div class="p-2 flex-even bd-highlight">
+          <h2>Visit us on Facebook!</h2>
+          <h5>Or check out our <a href="https://www.facebook.com/Sheffield-signs-1510717192492363/">Facebook</a> page and send us a message</h5>
         </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-4">
-        <h2>Feedback Info</h2>
         </div>
-      </div>
+          <div id="FeedbackPitch" class="d-flex flex-column bd-highlight">
+          <div class="bd-highlight"><h1>Used our services before?</h1></div>
+          <div class="bd-highlight"><h4>Let us know how we did by either leaving a review or recommending us on our Facebook page!</h4></div>
+        </div>
   </div>
   );
 };
