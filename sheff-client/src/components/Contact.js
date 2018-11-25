ba939514d2a1715be07c0e7fefca4fc7f6cb1c39
MrPhuzzles <tietjen.james92@gmail.com>
2018-11-25 14:51:10 -0500

    Contact.js update
    Got the framework of Contact page finished, ready for details to be put in. Partly styled, some center aligned some not to see which I prefer.

--- sheff-client/src/components/Contact.js
+++ sheff-client/src/components/Contact.js
@@ -3,21 +3,23 @@ import React from "react";
 const Contact = () => {
   return (
   <div class="container">
-
+    <div id="ContactHead">
+    <h1>Here's how you can get in touch with us...</h1>
+    </div>
       <div class="row">
         <div class="col-4">
-          Phone Inquiries
+          <h2>Give us a call!</h2>
         </div>
         <div class="col-4">
-          Email Inquiries
+          <h2>Send us an email!</h2>
         </div>
         <div class="col-4">
-          Facebook Page
+          <h2>Find us on Facebook!</h2>
         </div>
       </div>
       <div class="row justify-content-center">
         <div class="col-4">
-        Feedback Info
+        <h2>Feedback Info</h2>
         </div>
       </div>
   </div>
